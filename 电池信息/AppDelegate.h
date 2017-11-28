a6a5107735e9d6296c96cb3ed819e199ac49d9c6
MAc <mac@macdemacbook-air.local>
2017-11-28 15:21:58 +0800

    Initial Commit

---
+++ 电池信息/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  电池信息
+//
+//  Created by MAc on 2017/11/28.
+//  Copyright © 2017年 MAc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 电池信息/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  电池信息
+//
+//  Created by MAc on 2017/11/28.
+//  Copyright © 2017年 MAc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
